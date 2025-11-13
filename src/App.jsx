b8c3ca1badd8601c367f9865e1395b0371dbd5bc
KyllianDev <kyllianjourdan@outlook.com>
2025-11-13 20:33:12 +0100

    correction url img

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Header from './components/Header'
 import Letter from './components/Letter'
 
 const gifts = [
-  { id: 1, title: 'Apple watch serie 10 46mm', img: '/aw.png', link: 'https://www.boulanger.com/ref/1211660?offer=33dbc973-0369-48e0-94b6-38d54b4c59ed' },
-  { id: 2, title: 'Balance connectée apple', img: '/balance.jpeg', link: 'https://www.apple.com/fr/shop/product/hqyd2zm/a/body-smart-de-withings-balance-connect%C3%A9e-avec-analyse-corporelle-avanc%C3%A9e' },
-  { id: 3, title: 'Une porsche' , img: '/porsche.png', link: 'https://www.leboncoin.fr/recherche?category=2&text=porsche+911&kst=p&pi=da65663e-c695-4a67-8cb3-707efbbd288e' }
+  { id: 1, title: 'Apple watch serie 10 46mm', img: '/React-Christmas-Letter/aw.png', link: 'https://www.boulanger.com/ref/1211660?offer=33dbc973-0369-48e0-94b6-38d54b4c59ed' },
+  { id: 2, title: 'Balance connectée apple', img: '/React-Christmas-Letter/balance.jpeg', link: 'https://www.apple.com/fr/shop/product/hqyd2zm/a/body-smart-de-withings-balance-connect%C3%A9e-avec-analyse-corporelle-avanc%C3%A9e' },
+  { id: 3, title: 'Une porsche' , img: '/React-Christmas-Letter/porsche.png', link: 'https://www.leboncoin.fr/recherche?category=2&text=porsche+911&kst=p&pi=da65663e-c695-4a67-8cb3-707efbbd288e' }
 ];
 
 export default function App() {
